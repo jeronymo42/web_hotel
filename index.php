d2ba6f0674b4d50684c5e3fa5d710aaac16a78db
jeronymo42 <vladimir.ledovskikh@gmail.com>
2022-10-17 18:38:48 +0300

    First php page

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Glass Huts</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="css/normalize.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header>
+        <a class="hut-icon" href="#"><img src="img/logo.svg"></a>
+        <ul>
+            <li>главная</li>
+            <li>о хижине</li>
+            <li>окружение</li>
+            <li>бронирование</li>
+            <li>о нас</li>
+            <li>faq</li>
+        </ul>
+        <p class="btn-book">Забронировать</p>
+    </header>
+    <main>
+        <div class="intro">
+            <h1>Найдите покой в лесу</h1>
+            <p>Приезжайте и оставайтесь в уютной хижине недалеко от Сочи</p>
+        </div>
+
+    </main>
+    <?php phpinfo(); ?>
+    <script type="text/javascript" charset="utf-8" async
+        src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A08048c69442ff354caf03b41dc2c492e21d5a81af6e42e034a15d1e306767d5c&amp;width=500&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
+</body>
+
+</html>
